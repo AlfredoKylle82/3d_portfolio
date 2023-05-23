f58e93047f79366dd284e906ed21cef6d48859f7
Alfredo kylle Navarro <alfredokylle28@gmail.com>
2023-05-23 19:16:55 -0400

    Revert "4th commit: Fixed logo on experiences" This reverts commit 12525387b91981bc7cc2aa26cb2a5945c567ff89.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.dev 
               animate={{ 
-                y: [0, 25 , 0]
+                y: [0, 24, 0]
               }}
               transition={{
                 duration: 1.5,
