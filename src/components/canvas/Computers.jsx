8338e727137a72fe1d1b6a973f0c8d6dcbef649c
Alfredo kylle Navarro <alfredokylle28@gmail.com>
2023-05-23 19:24:57 -0400

    Revert "third commit: added more functionalities such as timeline"
    This reverts commit b84cc794033f4f1cbc9ac5fbc5ccc345bbd4aaa9.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,44 +4,21 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = ({ isMobile }) => {
+const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <spotLight position={[-20, 50, 10]}
-      angle ={0.12}
-      penumbra={1} 
-      intensity={1}/>
       <primitive object={computer.scene} 
-        scale={isMobile ? 0.5 : 0.75} 
-        position={isMobile ? [0, -3.0, -2.2] : [0, -3.25, -1.5]} 
+        scale={0.75} 
+        position={[0, -3.25, -1.5]} 
         rotation={[-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  // add a listener for changes to the screen size
-  useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
-
-    //set the initial value of the 'isMobile' state variable
-    setIsMobile(mediaQuery.matches);
-
-    // define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    }
-    // add the event listener
-    mediaQuery.addEventListener('change', handleMediaQueryChange);
-    // remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener('change', handleMediaQueryChange);
-    }
-  }, [])
   return (
     <Canvas 
       frameLoop="demand" 
@@ -54,7 +31,7 @@ const ComputersCanvas = () => {
          maxPolarAngle={Math.PI / 2}
          minPolarAngle={Math.PI / 2} 
          />
-         <Computers isMobile={isMobile}/>
+         <Computers />
       </Suspense>
 
     
